3fca36ff28cf9c25cbe77da4ddfc33c0157d2f57
David Craig <david@silverstripe.com>
2016-02-05 16:22:07 +1300

    Improve build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,61 +7,61 @@ var gulp = require('gulp'),
 	semver = require('semver'),
 	notify = require('gulp-notify');
 
-var paths = {
-	dist: './javascript/dist',
-	js: ['./javascript/src/**/*.js'],
-	scss: ['./javascript/src/**/*.scss'],
-	image: ['./javascript/src/img/**']
+var PATHS = {
+	JAVASCRIPT_DIST: './javascript/dist',
+	JAVASCRIPT_SRC: './javascript/src/**/*.js',
+	SCSS: './javascript/src/**/*.scss',
+	IMAGES: './javascript/src/img/**'
 };
 
+var isDev = typeof process.env.npm_config_development !== 'undefined';
+
 var nodeVersionIsValid = semver.satisfies(process.versions.node, packageJSON.engines.node);
+
+var browserifyOptions = { entries: './javascript/src/boot/index.js' };
 
 if (!nodeVersionIsValid) {
 	console.error('Invalid Node.js version. You need to be using ' + packageJSON.engines.node);
 	process.exit();
 }
 
-gulp.task('js:watch', function () {
-	gulp.watch(paths.js, ['js']);
-});
+if (isDev) {
+	browserifyOptions.debug = true;
+}
 
 gulp.task('js', function () {
-	browserify({
-		entries: './javascript/src/boot/index.js',
-		extensions: ['.js'],
-		debug: true
-	})
-	.transform(babelify)
-	.external('react')
-	.external('jQuery')
-	.external('i18n')
-	.external('silverstripe-component')
-	.external('react-dom')
-	.external('react-addons-test-utils')
-	.external('react-redux')
-	.external('redux')
-	.external('redux-thunk')
-	.bundle()
-	.on('error', notify.onError({
-		message: 'Error: <%= error.message %>',
-	}))
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest(paths.dist));
+	return browserify(browserifyOptions)
+		.transform(babelify)
+		.external('react')
+		.external('jQuery')
+		.external('i18n')
+		.external('silverstripe-component')
+		.external('react-dom')
+		.external('react-addons-test-utils')
+		.external('react-redux')
+		.external('redux')
+		.external('redux-thunk')
+		.external('page.js')
+		.bundle()
+		.on('error', notify.onError({ message: 'Error: <%= error.message %>' }))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(PATHS.JAVASCRIPT_DIST));
 });
 
 gulp.task('sass', function () {
-	gulp.src('./javascript/src/styles/main.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('sass:watch', function () {
-	gulp.watch(paths.scss, ['sass']);
+	return gulp.src('./javascript/src/styles/main.scss')
+		.pipe(sass().on('error', notify.onError({ message: 'Error: <%= error.message %>' })))
+		.pipe(gulp.dest(PATHS.JAVASCRIPT_DIST));
 });
 
 gulp.task('images', function () {
-	gulp.src(paths.image)
-		.pipe(gulp.dest(paths.dist + '/img'));
+	return gulp.src(PATHS.IMAGES)
+		.pipe(gulp.dest(PATHS.JAVASCRIPT_DIST + '/img'));
 });
 
-gulp.task('default', ['js:watch', 'sass:watch', 'js', 'sass', 'images']);
+gulp.task('default', ['js', 'sass', 'images'], function () {
+	if (isDev) {
+		gulp.watch(PATHS.JAVASCRIPT_SRC, ['js']);
+		gulp.watch(PATHS.SCSS, ['sass']);
+	}
+});
